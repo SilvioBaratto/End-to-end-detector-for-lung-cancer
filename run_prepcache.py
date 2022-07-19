76447a99bc4471367bf0a207b9543e15220df364
SilvioBaratto <silviobaratto@xps-13-9310.homenet.telecomitalia.it>
2022-07-19 16:31:55 +0200

    chapter 2

---
+++ run_prepcache.py
@@ -0,0 +1,33 @@
+import datetime
+
+from util.util import importstr
+from util.logconf import logging
+
+
+import os
+import shutil
+
+
+log = logging.getLogger('nb')
+
+def run(app, *argv):
+    argv = list(argv)
+    argv.insert(0, '--num-workers=4')  # <1>
+    log.info("Running: {}({!r}).main()".format(app, argv))
+    
+    app_cls = importstr(*app.rsplit('.', 1))  # <2>
+    app_cls(argv).main()
+    
+    log.info("Finished: {}.{!r}).main()".format(app, argv))
+
+# clean up any old data that might be around.
+# We don't call this by default because it's destructive, 
+# and would waste a lot of time if it ran when nothing 
+# on the application side had changed.
+def cleanCache():
+    shutil.rmtree('data-unversioned/cache')
+    os.mkdir('data-unversioned/cache')
+
+# cleanCache()
+
+run('prepcache.LunaPrepCacheApp')
