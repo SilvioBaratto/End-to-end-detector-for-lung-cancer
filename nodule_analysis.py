0a52efa0723c35f3f49e302850382de02172d51b
SilvioBaratto <silviobaratto@xps-13-9310.homenet.telecomitalia.it>
2022-07-19 13:09:13 +0200

    prova

--- nodule_analysis.py
+++ nodule_analysis.py
@@ -180,18 +180,13 @@ class NoduleAnalysisApp:
 
     def initModelPath(self, type_str):
         local_path = os.path.join(
-            # 'data-unversioned',
-            # 'part2',
             'models',
-            'p2ch13',#self.cli_args.tb_prefix,
             type_str + '_{}_{}.{}.state'.format('*', '*', 'best'),
         )
 
         file_list = glob.glob(local_path)
         if not file_list:
             pretrained_path = os.path.join(
-                # 'data',
-                # 'part2',
                 'models',
                 type_str + '_{}_{}.{}.state'.format('*', '*', '*'),
             )
